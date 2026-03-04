c6a5f17a06babf99b3cc04764f5dc2af30ba05d8
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2026-03-03 23:14:28 -0300

    FEAT - Adjustment to the connection with the database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
@@ -7,7 +8,7 @@ const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
 
-mongoose.connect('mongodb+srv://lima_db_user:gigagiga@node-store.qdyqkfh.mongodb.net/?appName=node-store')
+mongoose.connect(process.env.MONGO_URI);
 
 const Product = require('./models/product')
 
